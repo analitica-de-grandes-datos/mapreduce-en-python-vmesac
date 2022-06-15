e410b02765026bf057e1bab39e6f3e1bcffec6bd
vmesac <vmesac@unal.edu.co>
2022-06-14 23:03:58 -0500

    punto1

--- pregunta_01/mapper.py
+++ pregunta_01/mapper.py
@@ -1,3 +1,11 @@
 #
 # >>> Escriba el codigo del mapper a partir de este punto <<<
 #
+# Esta es la funcion que mapea la entrada a parejas (clave, valor)
+import sys
+if __name__ == "__main__":
+    
+    # itera sobre cada linea de codigo recibida a traves del flujo de entrada
+    for line in sys.stdin:
+        data = (line.split(",")[2])
+        sys.stdout.write("{}\t1\n".format(data))
