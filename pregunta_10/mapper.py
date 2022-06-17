827aebff2e6d1480f419499d18dd7f3e92de22ec
vmesac <vmesac@unal.edu.co>
2022-06-17 10:12:31 -0500

    final commit

--- pregunta_10/mapper.py
+++ pregunta_10/mapper.py
@@ -3,9 +3,12 @@
 #
 import sys
 if __name__ == "__main__":
-    
+ 
     # itera sobre cada linea de codigo recibida a traves del flujo de entrada
     for line in sys.stdin:
         data = line.split()
         letters = data[1].split(',')
-        sys.stdout.write("{},{}\n".format(data[0], letters))
+        #newdata = []
+        for letter in letters:
+            #newdata.append(data[0] + '-' + letter)
+            sys.stdout.write("{}\t{}\n".format(letter, data[0]))
--- pregunta_10/reducer.py
+++ pregunta_10/reducer.py
@@ -1,3 +1,37 @@
 #
 # >>> Escriba el codigo del reducer a partir de este punto <<<
 #
+import sys
+# Esta funcion reduce los elementos que tienen la misma clave
+#
+if __name__ == '__main__':
+
+    curkey = None
+    total = ''
+
+    # cada linea de texto recibida es una entrada clave \tabulador valor
+
+    for line in sys.stdin:
+
+        key, val = line.split("\t")
+        val = val.strip()
+
+        if key == curkey:
+            # No se ha cambiado de clave. Aca se acumulan los valores para la misma clave.
+            total = total + ',' + val
+            total = total.split(',')
+            total = sorted(int(i) for i in total)
+            total = ','.join(str(i) for i in total)
+
+        else:
+            # Se cambio de clave. Se reinicia el acumulador.
+            if curkey is not None:
+    
+                # una vez se han reducido todos los elementos con la misma clave se imprime el resultado en
+                # el flujo de salida
+                sys.stdout.write("{}\t{}\n".format(curkey, total))
+
+            curkey = key
+            total = val
+
+    sys.stdout.write("{}\t{}\n".format(curkey, total))
